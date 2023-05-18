7a567e787bafd939ca77e64ff5e1763eb44e9ee8
abdu95873 <abdu95873@gmail.com>
2023-05-19 01:08:38 +0600

    creating add toys form

--- src/Pages/AddToys/AddToys.jsx
+++ src/Pages/AddToys/AddToys.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddToys = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+
+
     return (
         <div>
-            
+            <form>
+                    <input {...register("toyName")} type="text" placeholder="Enter toyName"/>
+                    <input {...register("price")} type="text" placeholder="Enter price"/>
+                    <input {...register("rating")} type="text" placeholder="Enter rating"/>
+                    <input {...register("pictureUrl")} type="text" placeholder="Enter pictureUrl"/>
+                    <input {...register("quantity")} type="text" placeholder="Enter quantity"/>
+                    <input {...register("toyDetails")} type="text" placeholder="Enter toyDetails"/>
+
+                <div>
+                    <label>
+                        
+                    </label>
+                    <select {...register("subCategory")}>
+                        <option disabled selected>Select one</option>
+                        <option>Truck Lorry</option>
+                        <option>Dump Truck</option>
+                        <option>Mini Truck</option>
+                    </select>
+                </div>
+
+                <input value='REGISTER' type="submit" />
+            </form>
         </div>
     );
 };
