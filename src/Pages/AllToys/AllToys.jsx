2132a90898728d9e7cbdc3a80acae9e9cb751dbf
abdu95873 <abdu95873@gmail.com>
2023-05-19 03:14:52 +0600

    search done in all toys page

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
 
 const AllToys = () => {
     const allToyes = useLoaderData();
-    console.log(allToyes)
+    const [toys, setToys] = useState(allToyes);
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const handleSearch = (data) => {
+        fetch(`http://localhost:5000/trucks?toyName=${data?.toyName}`)
+            .then(res=>res.json())
+            .then(data=>setToys(data))
+    }
     return (
         <div>
-            <h1>alltoys</h1>
+            <form onSubmit={handleSubmit(handleSearch)}>
+                <input {...register("toyName")} type="text" placeholder="Enter price" />
+                <input value='REGISTER' type="submit" />
+            </form>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        toys?.map(toy => <>
+                            <tr>
+                                <td>{toy?.toyName}</td>
+                                <td>{toy?.price}</td>
+                                <td>
+                                    <Link to={`/edit/${toy?._id}`}>Edit</Link>
+                                    <button onClick={() => handleConfirmDelete(toy?._id)}>Delete</button>
+                                </td>
+                            </tr>
+                        </>)
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
